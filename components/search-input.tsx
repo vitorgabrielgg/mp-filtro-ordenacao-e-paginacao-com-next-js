0b0a04ba79e2cf0762167ca49fed7fd9d225425f
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-02-01 10:02:47 -0300

    feat: set the search query param

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,7 +1,29 @@
-import { Search } from 'lucide-react';
-import { Input } from '@/components/ui/input';
+"use client";
+
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { ChangeEvent, useState } from "react";
+import { deleteQueryParam, getQueryParam, setQueryParam } from "@/utils";
+import { useRouter } from "next/navigation";
 
 export default function SearchInput() {
+  const router = useRouter();
+  const searchParam = getQueryParam("search") ?? "";
+
+  const [search, setSearch] = useState(searchParam);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    let params;
+    if (!e.target.value) {
+      params = deleteQueryParam("search");
+    } else {
+      params = setQueryParam("search", e.target.value);
+    }
+
+    router.push(`?${params.toString()}`);
+    setSearch(e.target.value);
+  }
+
   return (
     <div className="relative">
       <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
@@ -9,6 +31,8 @@ export default function SearchInput() {
         type="search"
         placeholder="Busque por nome..."
         className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+        onChange={handleChange}
+        value={search}
       />
     </div>
   );
