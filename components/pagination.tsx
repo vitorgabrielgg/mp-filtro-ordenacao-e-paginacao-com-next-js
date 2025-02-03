81ffbaee43efb3b6eff3b52ad303b1507f1fb459
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-02-03 10:28:54 -0300

    feat: add links to navigate between the pages

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,3 +1,4 @@
+import { LinkType } from "@/@types";
 import {
   Pagination as PaginationComponent,
   PaginationContent,
@@ -6,39 +7,71 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from '@/components/ui/pagination';
+} from "@/components/ui/pagination";
+import { deleteQueryParam } from "@/utils";
+import { useRouter } from "next/navigation";
 
-export default function Pagination() {
+interface PaginationProps {
+  links: LinkType[] | undefined;
+}
+
+export default function Pagination({ links }: PaginationProps) {
+  const router = useRouter();
+  const params = deleteQueryParam("page") ?? "";
+
+  function handleRoute(url: string) {
+    if (!url.includes("undefined")) {
+      router.push(`?${url}`);
+    }
+  }
+
   return (
     <PaginationComponent>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious />
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink isActive={true}>1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>2</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>8</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>9</PaginationLink>
-        </PaginationItem>
-        <PaginationItem className="hidden md:inline-flex">
-          <PaginationLink>10</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext />
-        </PaginationItem>
+        {links?.map((link) => {
+          const pageIndex = link.url?.split("?")[1];
+          const url = !params.toString()
+            ? `${pageIndex}`
+            : `${pageIndex}&${params}`;
+
+          if (link.label.includes("Anterior")) {
+            return (
+              <PaginationItem key={link.label} className="cursor-pointer">
+                <PaginationPrevious onClick={() => handleRoute(url)} />
+              </PaginationItem>
+            );
+          }
+
+          if (link.label.includes("Próximo")) {
+            return (
+              <PaginationItem key={link.label} className="cursor-pointer">
+                <PaginationNext onClick={() => handleRoute(url)} />
+              </PaginationItem>
+            );
+          }
+
+          if (link.label.includes("...")) {
+            return (
+              <PaginationItem key={link.label}>
+                <PaginationEllipsis />
+              </PaginationItem>
+            );
+          }
+
+          return (
+            <PaginationItem
+              key={link.label}
+              className="hidden md:inline-flex cursor-pointer"
+            >
+              <PaginationLink
+                onClick={() => handleRoute(url)}
+                isActive={link.active}
+              >
+                {link.label}
+              </PaginationLink>
+            </PaginationItem>
+          );
+        })}
       </PaginationContent>
     </PaginationComponent>
   );
